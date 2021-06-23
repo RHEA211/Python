10fa66d914c3f7c84a6fac6ee0846ced0e43a332
Rhea <64091188+RHEA211@users.noreply.github.com>
2021-06-23 20:37:08 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,12 @@
+a=input("Enter the value of a- ")
+b=input("Enter the value of b- ")
+c=input("Enter the value of c- ")
+
+if (a>b and a>c):
+    print("A is the greatest")
+
+elif(b>c and b>a):
+    print("B is the greatest")
+
+else:
+    print("C is the greatest")
