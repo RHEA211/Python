c553647bed9aff55ff6468c68abeb63662e7d948
Rhea <64091188+RHEA211@users.noreply.github.com>
2021-06-23 20:29:38 +0530

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,12 @@
+
+a=input("Enter the value of A")
+b=input("Enter the value of B")
+
+if (a>b):
+    print("A is greater than B")
+
+elif(a<b):
+    print("A is smaller than B")
+
+else:
+    print("Both are equal" )
