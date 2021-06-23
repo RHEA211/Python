d5313cf28477fb3b4d7ddd2c0e5023379ee738be
Rhea <64091188+RHEA211@users.noreply.github.com>
2021-06-23 20:21:07 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,4 @@
+a=input("enter value of a -")
+b=input("enter value of b -")
+c =int(a)+int (b)
+print("addition of {0} and {1} is {2}".format(a,b,c))
---
+++ concatenate.py
@@ -0,0 +1,4 @@
+a=input("enter value of a -")
+b=input("enter value of b -")
+c =a+b
+print("addition of {0} and {1} is {2}".format(a,b,c))
